415db8dd7bd341ef1d9167ee8c1d7c6624e1eefa
Mahlet <mahilibra97@gmail.com>
2025-10-29 15:20:58 +0200

    Updated file structure

--- taskanalyser.py
+++ taskanalyser.py
@@ -2,3 +2,6 @@ class Team:
     def __init__(self, code, name):
         self.code =code
         self.name = name
+        self.member = []
+    def add_member(self, member):
+        self.member.append(member)
